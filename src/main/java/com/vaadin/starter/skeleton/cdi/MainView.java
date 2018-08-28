95b70f1da6983b1527cab563affca85f88678c78
Pekka <pekka@vaadin.com>
2018-08-28 13:10:04 +0300

    Simplify starter
    Removes html files and adds readmes as placeholders.
    Use vaadin-maven-plugin instead of flow one.

--- src/main/java/com/vaadin/starter/skeleton/cdi/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/cdi/MainView.java
@@ -1,7 +1,7 @@
 package com.vaadin.starter.skeleton.cdi;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -10,18 +10,16 @@ import javax.inject.Inject;
 /**
  * The main view contains a simple label element and a template element.
  */
-@HtmlImport("styles/shared-styles.html")
 @Route("")
 public class MainView extends VerticalLayout {
 
     @Inject
-    public MainView(ExampleTemplate template) {
-        // This is just a simple label created via Elements API
+    private MessageBean messageBean;
+
+    public MainView() {
         Button button = new Button("Click me",
-                event -> template.setValue("Clicked!"));
-        // This is a simple template example
-        add(button, template);
-        setClassName("main-layout");
+                event -> Notification.show(messageBean.getMessage()));
+        add(button);
     }
 
 }
