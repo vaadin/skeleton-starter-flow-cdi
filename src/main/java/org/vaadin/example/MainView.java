67df7cf4d0cdf4debf7f9bbeb00c16bcef26f579
Shadi Khani <shadi@vaadin.com>
2020-03-21 12:09:57 +0200

    Removing flow from PWA name

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -8,7 +8,6 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
